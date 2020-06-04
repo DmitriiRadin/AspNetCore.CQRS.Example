22fabe7049d9c30f647c10099d9da9b53f558e5a
Dmitrii Radin <dmitrii_radin@epam.com>
2020-06-04 17:27:44 +0400

    Initialized dbContext and renamed comment entities

--- Domain.Entities/Comment.cs
+++ Domain.Entities/Comment.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Entities
 {
-    public class Comments
+    public class Comment
     {
         public Guid Id { get; set; }
 
---
+++ Infrastructure.Data/StoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
