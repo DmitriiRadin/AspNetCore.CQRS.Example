2e4cdd6f3ffded59d4912f1b765f168101a96dfe
Dmitrii Radin <dmitrii_radin@epam.com>
2020-06-04 17:08:07 +0400

    Add project files.

---
+++ Application.Handlers/HandlersSetup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Handlers
+{
+    public static class HandlersSetup
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Infrastructure.Data/DataSetup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Data
+{
+    public static class DataSetup
+    {
+        public static IServiceCollection AddData(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
